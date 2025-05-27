e59ec504f23d0f13b49bed87f71902fe98616089
derrickSh43 <derrick.weil@gmail.com>
2025-05-27 16:04:15 +0700

    first commit

---
+++ 05-instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami                         = "ami-06b21ccaeff8ct686"  
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  #user_data                   = filebase64("userdata.sh")
+
+  tags = {
+    Name = "web-server"
+  }
+}
